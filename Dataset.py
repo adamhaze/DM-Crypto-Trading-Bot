eb227ea44a45dff91c98e9b1a8592ed863ed32c4
Adam Hayes <adha9240@colorado.edu>
2021-11-01 18:46:57 -0600

    initializing Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -5,8 +5,18 @@ import torch
 
 class CryptoDataset(Dataset):
 
-	def __init__(self, timeframe, labels, train=True):
+	def __init__(self, timeframe, indicators, labels, train=False, live=False):
 
 		self.train = train
+		self.live = live
 		self.tf = timeframe
+		self.indicators = indicators
 		self.labels = labels # compute labels in data loader?
+
+
+	def __len__(self):
+		return 0
+
+
+	def __getitem__(self, idx):
+		return 0
